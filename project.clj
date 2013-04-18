361277e08db97e156b172c5e65ce3ecdda2139c5
Aniket Pant <me@aniketpant.com>
2013-04-18 21:22:49 +0530

    first commit
    Signed-off-by: Aniket Pant <me@aniketpant.com>

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject learn-clojure "0.1.0-SNAPSHOT"
+  :description "Basic application to learn basic clojure"
+  :url "https://github.com/aniketpant/learn-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main learn-clojure.core)
